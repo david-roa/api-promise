52eb3a27d77f88f7605bd98cb12648630468b964
david-roa <jroa52@uan.edu.co>
2021-02-26 02:47:00 -0500

    add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello/hello.component";
+import { SpotifyService } from "./service/spotify/spotify.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, HelloComponent],
+  providers: [SpotifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
