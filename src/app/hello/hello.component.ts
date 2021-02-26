b48a357273181724b61f51225c485b3b080852e6
david-roa <jroa52@uan.edu.co>
2021-02-26 03:19:08 -0500

    2 files modified

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,8 +7,14 @@ import { SpotifyService } from "../service/spotify/spotify.service";
 })
 export class HelloComponent implements OnInit {
   @Input() name: string;
+  private artist = "7jy3rLJdDQY21OgRLCZ9sD";
+  public source: any;
 
   constructor(private spotifyService: SpotifyService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.spotifyService.getArtists(this.artist).then(data => {
+      this.source = data;
+    });
+  }
 }
